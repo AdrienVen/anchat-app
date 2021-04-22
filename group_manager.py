22ab860518beb4ff3a4c7272faf4c29ee21a7b04
Alex Gonsalves <ag6191@nyu.edu>
2021-04-22 23:51:53 +0800

    Added disconnect/exit chat functionality and refactored group_manager.py

--- group_manager.py
+++ group_manager.py
@@ -1,190 +1,79 @@
 class GroupManager:
+
+    class Client:
+        def __init__(self, socket, name):
+            self.socket = socket
+            self.name = name
+
+
     def __init__(self):
         self.groups = {}
-        self.user_name_to_socket = {}
-        self.user_socket_to_name = {}
 
 
     #
-    # add_user_socket - Adds a new user socket to the socket to name dictionary
+    # add_client - Attempts to add a client with a socket and name to a group, returns True only if the name is unused in that group
     #
-    def add_user_socket(self, socket, name = None):
-        self.user_socket_to_name[socket] = name
-
-
-    #
-    # remove_user_by_name - Removes a user by a non-None name from the socket to name and name to socket dictionaries
-    #
-    def remove_user_by_name(self, name):
-        if name != None & self.is_name_member(name):
-            socket = self.name_to_socket.pop(name)
-            if self.is_socket_member(socket):
-                del self.user_socket_to_name[socket]
-
-
-    #
-    # remove_user_by_socket - Removes a user by a socket from the socket to name and name to socket dictionaries
-    #
-    def remove_user_by_socket(self, socket):
-        if self.is_socket_member(socket):
-            name = self.user_socket_to_name.pop(socket)
-            if name != None & self.is_name_member(name):
-                del self.user_name_to_socket[name]
-
-
-    #
-    # is_name_member - Returns True if the given name belongs to a user
-    #
-    def is_name_member(self, name):
-        return (self.user_name_to_socket.get(name,"N/A") != "N/A")
-
-
-    #
-    # is_socket_member - Returns True if the socket belongs to a user
-    #
-    def is_socket_member(self, socket):
-        return socket in self.user_socket_to_name
-
-
-    #
-    # get_user_socket - Returns the socket that belongs to the given user name
-    #
-    def get_user_socket(self, name):
-        return self.user_name_to_socket.get(name)
-
-
-    #
-    # get_user_name - Returns the user name that belongs to the given socket
-    #
-    def get_user_name(self, socket):
-        return self.user_socket_to_name.get(socket)
-
-
-    #
-    # set_user_name - Sets the new user name to the value and key of the socket to name and name to socket dictionaries respectivley
-    #
-    def set_user_name(self, socket, name):
-        if socket in self.user_socket_to_name:
-            self.user_socket_to_name[socket] = name
-        
-        for old_name in self.user_name_to_socket.keys():
-            if self.user_name_to_socket[old_name] == socket:
-                del self.user_name_to_socket[old_name]
-        
-        self.user_name_to_socket[name] = socket
-
-
-    #
-    # is_name_available - Returns True if the name is not used by another user
-    #
-    def is_name_available(self, name):
-        return not name in self.user_name_to_socket
-
-
-    #
-    # does_group_exist - Returns True if the group exists
-    #
-    def does_group_exist(self, group):
-        return self.groups.get(group,"N/A") != "N/A"
-
-
-    #
-    # socket_join_group - Either adds a user's socket to an existing group or creates a new group
-    #
-    def socket_join_group(self, group, socket):
-        if self.does_group_exist(group):
-            if socket not in self.groups[group]:
-                self.groups[group].append(socket)
+    def  add_client(self, socket, name, group):
+        if self.groups.get(group) == None:
+            # create the group and add the client
+            self.groups[group] = [self.Client(socket, name)]
         else:
-            self.groups[group] = [socket]
+            # determine if the name has already been used
+            for client in self.groups[group]:
+                if client.name == name:
+                    return False 
+
+            # add the client to the group
+            self.groups[group].append(self.Client(socket, name))
+            
+        return True
 
 
     #
-    # name_join_group - Gets a socket by the given user name and calls the socket_join_group function
+    # remove_client_from_group - Removes a client by their name and/or socket from a given group
     #
-    def name_join_group(self, group, name):
-        socket = self.user_name_to_socket[name]
-        if socket != None:
-            self.socket_join_group(group, socket)
+    def remove_client_from_group(self, group, socket = None, name = None):
+        for client in self.groups.get(group, []):
+            # remove the client from the group
+            if client.name == name or client.socket == socket:
+                self.groups[group].remove(client)
+
+                # delete the group if there are no clients left inside
+                if len(self.groups[group]) == 0:
+                    del self.groups[group]
 
 
     #
-    # socket_in_group - Returns True if the group does exist and if that socket is in that group
+    # remove_client - Removes a client from a group by their socket and/or name
     #
-    def socket_in_group(self, group, socket):
-        return self.does_group_exist(group) & (socket in self.groups[group])
-
-
-    #
-    # name_in_group - Returns True if the group does exist and if the socket belonging to that user name is in that group
-    #
-    def name_in_group(self, group, name):
-        return self.socket_in_group(group, self.user_name_to_socket[name])
-
-
-    #
-    # socket_leave_group - Removes a socket from a group and deletes the group if there are no members left
-    #
-    def socket_leave_group(self, group, socket):
-        if self.does_group_exist(group):
-            self.groups[group].remove(socket)
-            self.reset_socket_name(socket)
-            if len(self.groups[group]) == 0:
-                del self.groups[group]
-
-    
-    #
-    # reset_socket_name - Removes a given socket user name
-    #
-    def reset_socket_name(self, socket):
-        del self.user_name_to_socket[self.get_user_name(socket)]
-        self.user_socket_to_name[socket] = None
-
-
-    #
-    # name_leave_group - Removes a socket belonging to that user name with the socket_leave_group function
-    #
-    def name_leave_group(self, group, name):
-        socket = self.user_name_to_socket[name]
-        if socket != None:
-            self.socket_leave_group(group, socket)
-
-
-    #
-    # disconnect_by_socket - Disconnects a user from their group by socket and removes them from the user dictionaries
-    #
-    def disconnect_by_socket(self, socket):
+    def remove_client(self, socket = None, name = None):
         for group in self.groups.keys():
-            if self.socket_in_group(group, socket):
-                self.groups[group].remove(socket)
+            # find the group the client belongs to
+            for client in self.groups[group]:
+                if client.name == name or client.socket == socket:
+                    #remove the client from the group
+                    self.groups[group].remove(client)
 
-        self.remove_user_by_socket
-
-
-    #
-    # disconnect_by_name - Disconnects a user from their group by socket/name and removes them from the user dictionaries
-    #
-    def disconnect_by_name(self, name):
-        socket = self.user_name_to_socket[name]
-        if socket != None:
-            for group in self.groups.keys():
-                if self.socket_in_group(group, socket):
-                    self.groups[group].remove(socket)
-
-        self.remove_user_by_name(name)
+                    # delete the group if there are no clients left inside
+                    if len(self.groups[group]) == 0:
+                        del self.groups[group]
 
 
     #
-    # get_group_by_socket - Returns a group name that contains the given socket
+    # get_group_names - Returns a list of all client names in a group
     #
-    def get_group_by_socket(self, socket):
-        for group in self.groups.keys():
-            if socket in self.groups[group]:
-                return group
+    def get_group_names(self, group):
+        names = []
+        for client in self.groups.get(group, []):
+            names.append(client.name)
+        return names
 
-    
+
     #
-    # get_group_members_by_socket - Returns a list of group members that are in the same group as the given socket
+    # get_group_sockets - Returns a list of all client sockets in a group
     #
-    def get_group_members_by_socket(self, socket):
-        return self.groups[self.get_group_by_socket(socket)]
+    def get_group_sockets(self, group):
+        sockets = []
+        for client in self.groups.get(group, []):
+            sockets.append(client.socket)
+        return sockets
--- server.py
+++ server.py
@@ -11,46 +11,80 @@ import group_manager as gm
 sio = socketio.Server()
 manager = gm.GroupManager()
 app = socketio.WSGIApp(sio, static_files={
-    '/': {'content_type': 'text/html', 'filename': 'login.html'}
+    '/': {'content_type': 'text/html', 'filename': 'client.html'}
 })
 
 
-
+#
+# connect - Registers when a client has connected
+#
 @sio.event
 def connect(sid, environ):
     print('connect ', sid)
     
-#adds a client sid to a groupchat data["group"]
+
+#
+# join - Attempts to add a client into a group and sends back a join status message
+#
 @sio.event
 def join(sid,data):
-    print(sid,"wants to join/create group:",data["group"])
-    
-    if manager.is_name_member(data["username"]):
-            # name already taken
-            sio.emit("joinStatus","0", room=sid)
-    else:
-        # name not taken
-        manager.set_user_name(sid, data["username"])
-        manager.socket_join_group(data["group"], sid)
+    if manager.add_client(sid, data["name"], data["group"]):
+        # user was successfully added to the group manager
+        print(data["name"] + " has JOINED group: " + data["group"])
         sio.emit("joinStatus","ok", room=sid)
 
+        # notify every member of a group that a client has joined (including this client)
+        for socket in manager.get_group_sockets(data["group"]):
+                sio.emit("addUser", data, to=socket)
+    else:
+        # username in this group has been taken
+        print(sid + " has FAILED to join group: " + data["group"])
+        sio.emit("joinStatus","0", room=sid)
 
-#transmits data from one sender(sid) to the rest of the group(data["group"]).
+
+#
+# recv - Transmits data from one client to the rest of the clients in the group
+#
 @sio.event
 def recv(sid, data):
-    print("received ", data["message"] + "from " + data["user"] + " for " + data["group"])
-    for socket in manager.groups[data["group"]]:
+    print("received '" + data["message"] + "' from " + data["name"] + " for group " + data["group"])
+    for socket in manager.get_group_sockets(data["group"]):
         if socket != sid:
-            sio.emit("recv", data, to=socket)
-            
-#Sends a message to all connected clients
+            sio.emit("recvMsg", data, to=socket)
+
+
+#
+# disconnectUser - Disconnects a user from their group and notifies the group members of it
+#
+@sio.event
+def disconnectUser(sid, data):
+    print("disconnecting " + data["name"] + " from group " + data["group"])
+
+    # tell all clients in this group that this client has disconnected    
+    for socket in manager.get_group_sockets(data["group"]):
+        if socket != sid:
+            sio.emit("removeUser", data, to=socket)
+
+    # remove this client from the group
+    manager.remove_client_from_group(data["group"], sid, data["name"])
+
+
+#     
+# send - Sends a message to all connected clients
+#
 @sio.event
 def send(data):
-    sio.emit("recv", {"server": str(data)})
+    sio.emit("recvMsg", {"server": str(data)})
 
+
+#
+# disconnect - Prints that a socket has disconnected (is this automatic? if so the sid should be removed from the manager and a message sent to everyone in that group)
+#
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
 
+
+    
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('localhost', 65432)), app)

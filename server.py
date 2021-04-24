39f67b26891c70e16ebc3b9dc04365ac1b2ef86d
Adrien Ventugol <agv261@nyu.edu>
2021-04-24 22:34:40 +0200

    Update server.py

--- server.py
+++ server.py
@@ -123,7 +123,7 @@ def disconnect(sid):
 
     
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=65432)
+    sio.run(app, host='0.0.0.0', port=65432)
     
 #if __name__ == '__main__':
 #    port = int(os.environ.get('PORT', 8080))
